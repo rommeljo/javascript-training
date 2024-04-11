d91f326c97fae78bad184fc58700a06537f31ec6
rommeljo <johnrommel031@gmail.com>
2024-04-11 17:09:46 +0300

    initial push

---
+++ binding.js
@@ -0,0 +1,16 @@
+//getting js to html
+//let f_name="john"
+//let s_name="kiarie"
+//let full_name=f_name+s_name
+//document.getElementbyId("first").innerHTML=full_name
+
+//getting html to JS
+
+
+
+ let form=document.getElementById("myform")
+
+ form.addEventListener("submit")
+
+
+ 
